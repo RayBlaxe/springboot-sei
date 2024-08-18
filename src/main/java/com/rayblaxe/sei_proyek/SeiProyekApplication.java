62a98928e37a89b7d444d63e0426613f8fa70733
rayblaxe <raihan21ti@mahasiswa.pcr.ac.id>
2024-08-18 23:50:04 +0700

    database proyek relation

---
+++ src/main/java/com/rayblaxe/sei_proyek/SeiProyekApplication.java
@@ -0,0 +1,13 @@
+package com.rayblaxe.sei_proyek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeiProyekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeiProyekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rayblaxe/sei_proyek/SeiProyekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayblaxe.sei_proyek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeiProyekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
